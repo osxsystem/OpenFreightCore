d61d023d213c4ade1c706e40ed8a0aac657b46a0
Hugues <osxsystem2014@gmail.com>
2023-07-18 20:32:36 +0700

    networking example

--- Sources/OpenFreightCore/Networking.swift
+++ Sources/OpenFreightCore/Networking.swift
@@ -11,5 +11,26 @@ import Foundation
 extension OpenFreightCore {
   public class Networking {
     
+    
+    /// Responsible for handling all networking calls
+    /// - Warning: Must create before using any public APIs
+    public class Manager {
+      public init() {}
+      
+      private let session = URLSession.shared
+      
+      public func loadData(from url: URL, completetionHandler: @escaping (NetworkResult<Data>) -> Void) {
+        let task = session.dataTask(with: url) { data, response, error in
+          let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+          completetionHandler(result)
+        }
+        task.resume()
+      }
+    }
+    
+    public enum NetworkResult<V> {
+      case success(V)
+      case failure(Error?)
+    }
   }
 }
--- Tests/OpenFreightCoreTests/OpenFreightNetworkingTest.swift
+++ Tests/OpenFreightCoreTests/OpenFreightNetworkingTest.swift
@@ -6,8 +6,28 @@
 //
 
 import XCTest
+import OpenFreightCore
 
 final class OpenFreightNetworkingTest: XCTestCase {
+  
+  func testLoadDataCall() {
+    let manager = OpenFreightCore.Networking.Manager()
+    let expectation = XCTestExpectation(description: "Called Networking for data")
+    guard let url = URL(string: "https://raywenderlich.com") else {
+      return XCTFail("Error! Could not create URL properly")
+    }
+    
+    manager.loadData(from: url) { result in
+      expectation.fulfill()
+      switch result {
+        case .success(let returnedData):
+          XCTAssertNotNil(returnedData, "Response data is nil")
+        case .failure(let error):
+          XCTFail(error?.localizedDescription ?? "error from result")
+      }
+    }
+    wait(for: [expectation], timeout: 5)
+  }
   
   override func setUpWithError() throws {
     // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,22 +37,7 @@ final class OpenFreightNetworkingTest: XCTestCase {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
   }
   
-  func testExample() throws {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-    // Any test you write for XCTest can be annotated as throws and async.
-    // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-    // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-  }
-  
-  func testPerformanceExample() throws {
-    // This is an example of a performance test case.
-    self.measure {
-      // Put the code you want to measure the time of here.
-    }
-  }
-  
   static var allTests = [
-    ("testExample", testExample)
+    ("testLoadDataCall", testLoadDataCall)
   ]
 }
