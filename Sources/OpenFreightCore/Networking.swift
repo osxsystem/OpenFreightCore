cdb8b2844d11941fae63c92bd441404b8ad80a15
Hugues <osxsystem2014@gmail.com>
2023-07-19 22:32:31 +0700

    Refactor network session

--- Sources/OpenFreightCore/Networking.swift
+++ Sources/OpenFreightCore/Networking.swift
@@ -8,23 +8,41 @@
 import Foundation
 
 
+protocol NetworkSession {
+  func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void)
+}
+
+extension URLSession: NetworkSession{
+  func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+    let task = dataTask(with: url) { data, _, error in
+      completionHandler(data, error)
+    }
+    
+    task.resume()
+  }
+  
+}
+
 extension OpenFreightCore {
   public class Networking {
-    
-    
     /// Responsible for handling all networking calls
     /// - Warning: Must create before using any public APIs
     public class Manager {
       public init() {}
       
-      private let session = URLSession.shared
+      internal var session: NetworkSession = URLSession.shared
       
+      
+      /// Calls to the network to retrieve data
+      /// - Parameters:
+      ///   - url: URL location from which to get data
+      ///   - completetionHandler: call back in result
       public func loadData(from url: URL, completetionHandler: @escaping (NetworkResult<Data>) -> Void) {
-        let task = session.dataTask(with: url) { data, response, error in
+        session.get(from: url) { data, error in
           let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
           completetionHandler(result)
         }
-        task.resume()
+
       }
     }
     
--- Tests/OpenFreightCoreTests/OpenFreightNetworkingTest.swift
+++ Tests/OpenFreightCoreTests/OpenFreightNetworkingTest.swift
@@ -6,16 +6,29 @@
 //
 
 import XCTest
-import OpenFreightCore
+@testable import OpenFreightCore
+
+class NetworkSessionMock : NetworkSession {
+  var data: Data?
+  var error: Error?
+  func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+    completionHandler(data, error)
+  }
+  
+  
+}
 
 final class OpenFreightNetworkingTest: XCTestCase {
   
   func testLoadDataCall() {
     let manager = OpenFreightCore.Networking.Manager()
+    let session = NetworkSessionMock()
+    manager.session = session
     let expectation = XCTestExpectation(description: "Called Networking for data")
-    guard let url = URL(string: "https://raywenderlich.com") else {
-      return XCTFail("Error! Could not create URL properly")
-    }
+    let data = Data([0, 1, 0, 1])
+    session.data = data
+    let url = URL.init(fileURLWithPath: "url")
+    
     
     manager.loadData(from: url) { result in
       expectation.fulfill()
